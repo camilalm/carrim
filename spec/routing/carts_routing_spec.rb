bc9c58dd42c803db88c654278e85a975aeb0d2b9
Camila <camilalm@gmail.com>
2025-06-13 19:51:31 -0400

    Resolve some TODOs

--- spec/routing/carts_routing_spec.rb
+++ spec/routing/carts_routing_spec.rb
@@ -2,12 +2,20 @@ require "rails_helper"
 
 RSpec.describe CartsController, type: :routing do
   describe 'routes' do
-    xit 'routes to #show' do
+    it 'routes to #show' do
       expect(get: '/cart').to route_to('carts#show')
     end
 
-    xit 'routes to #create' do
-      pending "#TODO: Escreva um teste para validar a criação de um carrinho #{__FILE__}"
+    it 'routes to #create' do
+      expect(post: '/cart').to route_to('carts#add_item')
+    end
+
+    it 'routes to #delete' do
+      expect(delete: '/cart/1').to route_to(
+        controller: 'carts',
+        action: 'remove_cart_item',
+        product_id: '1'
+      )
     end
 
     it 'routes to #add_item via POST' do
